fb384b774c52d0348b29da727ccc903ed4ead61a
ali esmaeily <65107389+CounterMader@users.noreply.github.com>
2021-11-10 15:02:05 +0330

    Adding initial header's

---
+++ LCD.h
@@ -0,0 +1,40 @@
+/*
+ * LCD.h
+ *
+ * Created: 11/10/2021 9:55:36 AM
+ *  Author: ESMAEILY
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define WAIT_TYPE 1									//Checking busy flag = 1, Waiting a constant time = 0 
+#define CONST_DELAY 2								//time in ms
+#define RS 0
+#define RW 1
+#define E 2
+#define LCD_DBUS_PORT PORTA
+#define LCD_DBUS_DDRx DDRA
+#define LCD_DBUS_PINx PINA
+#define LCD_CBUS_PORT PORTB
+#define LCD_CBUS_DDRx DDRB
+
+
+void comnwrt(unsigned char command);
+void datawrt(unsigned char data);
+void dispdata(char a[]);
+#if(WAIT_TYPE)
+	void wait_flag();
+#endif
+void lcd_init();
+void kprintf(const char *fmt, ...);
+void kputunum(unsigned int v);
+void kputnum(int v);
+static void putdigit0(unsigned char c);
+static void putdigit(unsigned char c, unsigned char *flag);
+void kputhexbyte(unsigned int v);
+void kputhex(unsigned int v);
+
+
+#endif /* LCD_H_ */
